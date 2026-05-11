38ede4d25c6325fd7bf7add049fe12a1d74b0837
JenniferXX-cheep <jennifer.dqin@education.nsw.gov.au>
2026-05-11 12:40:57 +1000

    added functions to loan

---
+++ library_project/loan.py
@@ -0,0 +1,16 @@
+class Loan():
+    def __init__(self, book, borrower, borrow_date, due_date, returned):
+        self.book = book
+        self.borrower = borrower
+        self.borrow_date = borrow_date
+        self.due_date = due_date
+        self.returned = returned
+
+    def mark_returned():
+        pass
+
+    def is_overdue():
+        pass
+
+    def calculate_fine():
+        pass
