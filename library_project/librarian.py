9424fbbd4fe2bdeab43c9320d30c8f2c0067bd56
JenniferXX-cheep <jennifer.dqin@education.nsw.gov.au>
2026-05-11 12:38:51 +1000

    added functions to librarian

---
+++ library_project/librarian.py
@@ -0,0 +1,16 @@
+class Librarian():
+    def __init__(self, name, staff_id):
+        self.name = name
+        self.staff_id = staff_id
+
+    def issue_loan(library, borrower, book):
+        pass
+
+    def accept_return(library, loan):
+        pass
+
+    def add_book(library, book):
+        pass
+
+    def register_borrower(library, borrower):
+        pass
