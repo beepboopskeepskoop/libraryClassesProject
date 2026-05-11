4df156611966920de4414ce1f031fd9b329b8db9
JenniferXX-cheep <jennifer.dqin@education.nsw.gov.au>
2026-05-11 12:38:17 +1000

    added functions to persistence manager

---
+++ library_project/persistence_manager.py
@@ -0,0 +1,9 @@
+class PersistenceManager():
+    def __init__(self, filename):
+        self.filename = filename
+
+    def save_library(library):
+        pass
+
+    def load_library():
+        pass
