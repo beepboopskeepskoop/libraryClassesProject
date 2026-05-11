0677ac12c2b3801879a5c25007946a3c580092bd
Beepboop Skeepskoop <katelynsirimat@gmail.com>
2026-05-11 12:23:50 +1000

    added book class, init, display

--- library_project/book.py
+++ library_project/book.py
@@ -1,1 +1,12 @@
-# book
+class Book():
+    def __init__(self, book_id, title, author, date):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.date = date
+
+    def display(self):
+        print(f"ID - {self.book_id}")
+        print(f"TITLE - {self.title}")
+        print(f"AUTHOR - {self.author}")
+        print(f"BORROWED - {self.borrowed}")
