8c944357f1d75405883eb9e90093459b8d663d93
JenniferXX-cheep <jennifer.dqin@education.nsw.gov.au>
2026-05-11 12:23:54 +1000

    i added stuff for the library class ehe

---
+++ library_project/library.py
@@ -0,0 +1,22 @@
+class Library():
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+        self.borrowers = []
+        self.loans = []
+        self.librarians = []
+
+    def add_book(self, book):
+        pass
+
+    def register_borrower(self, borrower):
+        pass
+
+    def create_loan(self, borrower, book):
+        pass
+
+    def return_book(self, loan):
+        pass    
+
+    def search_by_title(self, title):
+        pass
