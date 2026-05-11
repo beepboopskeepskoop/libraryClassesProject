fa63fea6ae448b5ba9aa7638c749d1723965e41f
JenniferXX-cheep <jennifer.dqin@education.nsw.gov.au>
2026-05-11 12:19:55 +1000

    i have shoved in some classes and functions

--- library_project/borrower.py
+++ library_project/borrower.py
@@ -1,1 +1,14 @@
-print("hello")
+class Borrower():
+    def __init__(self, name, borrower_id, loans):
+        self.name = name
+        self.borrower_id = borrower_id
+        self.loans = loans
+
+    def add_loan(self, loan):
+        pass
+
+    def list_current_loans(self):
+        pass
+
+    def has_overdue_loans(self):
+        pass
