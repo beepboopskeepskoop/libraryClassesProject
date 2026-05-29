fa1872341d79cc97e0b46a8aac78b67c9374f8e5
JenniferXX-cheep <jennifer.dqin@education.nsw.gov.au>
2026-05-29 12:44:39 +1000

    need to test loan, added comments to borrower

--- library_project/borrower.py
+++ library_project/borrower.py
@@ -5,14 +5,14 @@ class Borrower():
         self.loans = []
 
     def add_loan(self, loan):
-        self.loans.append(loan)
+        self.loans.append(loan) #when they borrow something/loan something add the book to their list of loans
 
     def list_current_loans(self):
-        print(f"Borrower: {self.name} (ID: {self.borrower_id})")
+        print(f"Borrower: {self.name} (ID: {self.borrower_id})") #format for loan display
         for loan in self.loans:
-            loan.display()
+            loan.display() #display all loans
 
     def has_overdue_loans(self):
-        for loan in self.lones:
-            if loan.is_overdue():
-                return True
+        for loan in self.lones: #for every loan they have in their loan list
+            if loan.is_overdue(): #check if the loan is overdue
+                return True #if any book is overdue, return that they do have overdue books
